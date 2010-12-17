8eb3ac9906ad58d9663d711755361ba2829180df
Evgeny Goldin <evgenyg@gmail.com>
2010-12-17 17:48:57 +0200

    Timer - done

--- src/main/groovy/Timer.groovy
+++ src/main/groovy/Timer.groovy
@@ -3,32 +3,26 @@
  * Reads commands to execute from stdin, outputs their average run time to stdout
  */
 
+final def nTimes       = 10
+final def inputStream  = args.length ? new File( args[ 0 ] ).newInputStream() : System.in
+final def tempFile     = File.createTempFile( "timer-temp", ".bat" )
+final def tempFilePath = tempFile.absolutePath
+
+
 /**
- * Accepts a closure, runs it and returns its runtime (in microseconds).
+ * Executes each line
  */
-def measureNano =
-{
-    long t = System.nanoTime()
-    it()
-    return ( System.nanoTime() - t ).intdiv( 1000 )
-}
-
-final def nTimes     = (( args.length > 0 ) ? args[ 0 ] as int : 10 )
-final def env        = System.getenv().collect{ key, value -> "$key=$value" } as String[]
-final def currentDir = new File( System.getProperty( "user.dir" ))
-final def tempFile   = File.createTempFile( "timer-temp", ".bat" )
-
-System.in.eachLine( 'UTF-8' )
+inputStream.eachLine( 'UTF-8' )
 {
     String command ->
 
     tempFile.write( command )
-    Runtime.getRuntime().exec( tempFile.absolutePath, env, currentDir )
+    nTimes.times { tempFilePath.execute() }
 
-    def timeNano  = measureNano { nTimes.times { command.execute().text }}.intdiv( nTimes )
-    def timeMicro = timeNano.intdiv( 10.power( 3 ))
-    def timeMilli = timeNano.intdiv( 10.power( 6 ))
-    println "[$command]: [$timeMicro] microseconds or [$timeMilli] milliseconds"
+    long t = System.currentTimeMillis()
+    nTimes.times { tempFilePath.execute() }
+    println "[$command]\t${( System.currentTimeMillis() - t ) / ( nTimes )}"
 }
 
+inputStream.close()
 assert tempFile.delete()
