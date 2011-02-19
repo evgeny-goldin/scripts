34e249988a4b2a4f7bc14ec110e7318dd2bb4b56
Evgeny Goldin <evgenyg@gmail.com>
2011-02-19 02:45:24 +0200

    Switching to splitWith() and fixing regex pattern

--- src/main/groovy/MW.groovy
+++ src/main/groovy/MW.groovy
@@ -1,20 +1,25 @@
 
+@GrabResolver( name='com.goldin', root='http://evgeny-goldin.org/artifactory/libs-releases/' )
+@Grab( group='com.goldin', module='gcommons', version='0.5' )
+import com.goldin.gcommons.GCommons
+
+GCommons.general() // To trigger MOP updates
+
+
 /**
  * Takes care of MediaWiki spacing between the sections: leaves two empty lines after each /=(+) section =(+)/.
  * Usage: groovy MW.groovy < input.txt > output.txt
  */
 
-def lines = []
-System.in.eachLine( 'UTF-8' ) { lines << it }
-
+def lines = System.in.splitWith( 'eachLine' )
 lines.eachWithIndex {
     String line, int index ->
 
-    if (( index > 1 ) && ( line ==~ /(=+)[^=]+\1/ )) // == Something Anything ==
+    if (( index > 1 ) && ( line ==~ /\s*(=+)[^=]+\1\s*/ )) // == Something Anything ==
     {
         if ( lines[ index - 1 ] ) println ""
         if ( lines[ index - 2 ] ) println ""
     }
 
-    println line
+    println line.replaceAll( /\s*$/, '' )
 }
