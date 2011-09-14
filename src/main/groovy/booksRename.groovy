1b80b1dd0127dce6859d793fd3934425974dd5b8
Evgeny Goldin <evgenyg@gmail.com>
2011-09-14 17:53:31 +0300

    Books rename scripts added

---
+++ src/main/groovy/booksRename.groovy
@@ -0,0 +1,28 @@
+
+def publishers = 'Oreilly Packtpub No.Starch Pragmatic'.tokenize()
+def months     = 'May Jul Aug'.tokenize()
+
+new File( '.' ).listFiles().findAll{ it.file && it.name.endsWith( '.pdf' ) }.
+                            each {
+                                File f ->
+                                def newName = f.name.
+                                              replaceAll  ( /\d{4}/, ''    ).
+                                              replaceFirst( /pdf$/,  ''    ).
+                                              replaceAll  (( publishers + months ).collect{ /\Q$it\E/ }.join( '|' ), '' ).
+                                              replace     ( '.',    ' '    ).
+                                              trim()
+
+                                def newFile = new File( "${newName}.pdf" )
+
+                                if ( newFile.file )
+                                {
+                                    println "! [$newFile.name] already exists, [$f.name] is not renamed"
+                                }    
+                                else
+                                {
+                                    assert   f.renameTo ( newFile )
+                                    println "[$f.name] => [$newFile.name]"
+                                }
+
+                            }
+
