1e06a7cddd0611d355a13305190b115f5ab1dfc8
Evgeny Goldin <evgenyg@gmail.com>
2011-10-05 17:52:06 +0200

    Browsers scripts added

---
+++ src/main/batch/browsers-evening.bat
@@ -0,0 +1,3 @@
+start C:\Users\evgeny\AppData\Local\Google\Chrome\Application\chrome.exe https://jira.clearforest.com/secure/Dashboard.jspa https://www.google.com/calendar/render?gsessionid=OK https://mail.google.com/ http://www.google.com/reader/view/ http://news.ycombinator.com/ http://goo.gl/BE9e4 https://plus.google.com/
+
+
