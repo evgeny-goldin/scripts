0256c26de29f27a13a858d9aa3e0f90acec8756a
Evgeny Goldin <evgenyg@gmail.com>
2011-10-21 04:08:56 +0200

    testY2m script added

---
+++ src/test/groovy/testY2m.groovy
@@ -0,0 +1,54 @@
+
+@GrabResolver( name='com.goldin', root='http://evgeny-goldin.org/artifactory/repo/' )
+@Grab('com.goldin:gcommons:0.5.3.4')
+@GrabExclude('commons-net:commons-net')
+@GrabExclude('org.codehaus.groovy:groovy-all')
+import com.goldin.gcommons.GCommons
+import com.goldin.gcommons.beans.ExecOption
+
+
+File testArchive = new File( '../resources/y2m/jetbrains-issues.zip' ).canonicalFile
+File y2m         = new File( '../../main/groovy/y2m.groovy' ).canonicalFile
+
+assert [ testArchive, y2m ].every{ it.file }
+
+GCommons.file().with { GCommons.general().with {
+
+    File tempDir  = tempDirectory()
+    File testData = new File( tempDir, 'jetbrains-issues.csv' ).canonicalFile
+
+    try
+    {
+        unpack( testArchive, tempDir )
+        assert testData.file
+
+        runTest( y2m, testData, [],                                                                   'jetbrains-issues-1.txt' )
+        runTest( y2m, testData, [ "Issue Id, Subsystem, Type, State, Summary" ],                      'jetbrains-issues-2.txt' )
+        runTest( y2m, testData, [ "Issue Id, Subsystem, Summary, Description" ],                      'jetbrains-issues-3.txt' )
+        runTest( y2m, testData, [ "Issue Id, Type, State, Summary", "Type, State, Summary" ],         'jetbrains-issues-4.txt' )
+        runTest( y2m, testData, [ "Issue Id, Type, State, Summary", "Type, State, Summary", 'true' ], 'jetbrains-issues-5.txt' )
+    }
+    finally
+    {
+        delete( tempDir )
+    }
+}}
+
+
+
+def runTest( File y2m, File testData, List<String> args, String testResultPath )
+{
+    def    t          = System.currentTimeMillis()
+    File   testResult = new File( "../resources/y2m/$testResultPath" ).canonicalFile
+    assert testResult.file
+
+    final PrintStream           out  = System.out
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream()
+
+    System.out = new PrintStream( baos )
+    new GroovyShell().run( y2m, ([ 'http://youtrack.jetbrains.net/', testData.path ] + args ) as List )
+    assert baos.toString() == testResult.text
+    System.out = out
+
+    println "$testResultPath - Ok ([${ System.currentTimeMillis() - t }] ms)"
+}
