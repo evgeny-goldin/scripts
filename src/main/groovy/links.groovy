147d1328b0d252d28d70d96932aacc76e999a547
Evgeny Goldin <evgenyg@gmail.com>
2011-11-20 00:58:51 +0200

    links.groovy - work in progress

--- src/main/groovy/links.groovy
+++ src/main/groovy/links.groovy
@@ -16,9 +16,10 @@ final int        maxWidth  = 80
 final File       directory = GCommons.verify().directory( new File( args[ 0 ] ))
 final String     pattern   = ( args.size() > 1 ? args[ 1 ] : '**' )
 final List<File> files     = GCommons.file().files( directory, [ pattern ] )*.canonicalFile
-final Map        headings  = files.inject( [:] ){ Map m, File f -> m[ f.name - GCommons.file().extension( f ) - '.' ] = processInternal( f ); m }
+final int        maxName   = files*.path*.size().max()
+final Map        headings  = files.inject( [:] ){ Map m, File f -> m[ f.name - GCommons.file().extension( f ) - '.' ] = processInternal( f, maxName ); m }
 
-files.each { File f -> processExternal( f, headings )}
+files.each { File f -> processExternal( f, headings, maxName )}
 
 println "[${ files.size()}] file${ GCommons.general().s( files.size())} processed"
 
@@ -29,46 +30,53 @@ println "[${ files.size()}] file${ GCommons.general().s( files.size())} processe
  * @param f file to check
  * @return headings found in a file
  */
-Set<String> processInternal ( File f )
+Set<String> processInternal ( File f, int maxNameLength )
 {
     assert f.file
+    print String.format( "%-${ maxNameLength + 2 }s - ", "[$f]" )
 
-    println "Processing internal links of [$f]"
-    String text               = f.getText( 'UTF-8' ).replaceAll( '\uFEFF', '' )
-    Set<String> headings      = text.findAll( /(?m)^\s*=+\s*([^=]+)\s*=+\s*$/ ){       // "= Text =" => "Text"
+    String       text     = f.getText( 'UTF-8' ).replaceAll( '\uFEFF', '' )
+    List<String> headings = text.findAll( /(?m)^\s*=+ +(.+?) +=+\s*$/ ) {  // "= Text =" => "Text"
         it[ 1 ].trim().
-        replaceAll( /\[.+?\s+(.+?)\]/ ){ it[ 1 ] }.                                  // [url Text1], [url Text2] => Text1, Text2
-        replaceAll( /'{2,}|<code>|<\/code>/, ''  )                                   // <code>'''Text'''</code>  => Text
-    } as Set
+        replaceAll( /\[.+?\s+(.+?)\]/ ){ it[ 1 ] }.                        // [url Text1], [url Text2] => Text1, Text2
+        replaceAll( /'{2,}|<code>|<\/code>/, ''  )                         // <code>'''Text'''</code>  => Text
+    }
 
     Set<String> internalLinks = text.findAll( /\[\[#(.+?)\|.+?\]\]/ ){ it[ 1 ] } as Set // [[#Text|description]] => Text
     assert    ( internalLinks || ( ! text.contains( '[[#' )))
 
-    for ( link in internalLinks )
-    {
-        assert headings.contains( link ), "Internal link [$link] references non-existing section"
+    internalLinks.findAll{ ! headings.contains( it ) }.with {
+        assert ( ! delegate ), "Broken internal links:\n* [${ delegate.join( ']\n* [' )}]"
     }
 
+    println String.format( "%-2d internal link%s", internalLinks.size(), GCommons.general().s( internalLinks.size()))
     headings
 }
 
 
-
-void processExternal( File f, Map<String, Set<String>> headings )
+void processExternal( File f, Map<String, Set<String>> headings, int maxNameLength )
 {
     assert f.file && headings
 
-    println "Processing external links of [$f]"
+    print String.format( "%-${ maxNameLength + 2 }s - ", "[$f]" )
     String      text          = f.getText( 'UTF-8' ).replaceAll( '\uFEFF', '' )
     Set<String> externalLinks = text.findAll( /\[(http.+?)\s+.+?\]/ ){ it[ 1 ] } as Set // [url text] => url
     assert    ( externalLinks || ( ! text.contains( '[http' )))
 
-    for ( link in externalLinks.findAll { it.contains( '#' ) } )
-    {
-        def ( String fileName, String anchor ) = link.findAll( /\/([^\/]+)#(.+?)$/ ){ it[ 1, 2 ] }[ 0 ]
-        if ( headings.containsKey( fileName ))
-        {
-            assert headings[ fileName ].contains( anchor ), "External link [$fileName#$anchor] references non-existing section"
-        }
+    externalLinks.findAll { it.contains( '#' ) }.
+                  findAll {
+                      def ( String fileName, String anchor ) = it.findAll( /\/([^\/]+)#(.+?)$/ ){
+                          it[ 1, 2 ]
+                      }[ 0 ]
+                      anchor = anchor.replace( '.3C', '<' ).
+                                      replace( '.3E', '>' ).
+                                      replace( '.22', '"' ).
+                                      replace( '.2C', ',' ).
+                                      replace( '_',   ' ' )
+                      headings.containsKey( fileName ) && ( ! headings[ fileName ].contains( anchor ))
+                  }.with {
+        assert ( ! delegate ), "Broken external links:\n* [${ delegate.join( ']\n* [' )}]"
     }
+
+    println String.format( "%-2d external link%s", externalLinks.size(), GCommons.general().s( externalLinks.size()))
 }
