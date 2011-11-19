e41a61049e8bfe383702f1e925a1db40ff908d6e
Evgeny Goldin <evgenyg@gmail.com>
2011-11-19 04:37:37 +0200

    links.groovy - work in progress

--- src/main/groovy/links.groovy
+++ src/main/groovy/links.groovy
@@ -8,19 +8,67 @@ import com.goldin.gcommons.GCommons
 
 
 /**
- * Validates MediaWiki internal and external links in files/directories specified
+ * Validates MediaWiki internal and external links in the directory specified
  */
 
-assert args.size() == 2, 'Two arguments exepcted: <directory> <files include pattern>'
+assert args, 'Arguments exepcted: <directory> [files include pattern]'
+final int        maxWidth  = 80
 final File       directory = GCommons.verify().directory( new File( args[ 0 ] ))
-final String     pattern   = args[ 1 ]
-final List<File> files     = GCommons.file().files( directory, [ pattern ] )*.canonicalFile.collect{ File f -> process( f ) }
+final String     pattern   = ( args.size() > 1 ? args[ 1 ] : '**' )
+final List<File> files     = GCommons.file().files( directory, [ pattern ] )*.canonicalFile
+final Map        headings  = files.inject( [:] ){ Map m, File f -> m[ f.name - GCommons.file().extension( f ) - '.' ] = processInternal( f ); m }
+
+files.each { File f -> processExternal( f, headings )}
 
 println "[${ files.size()}] file${ GCommons.general().s( files.size())} processed"
 
 
-File process ( File f )
+/**
+ * Processes the file specified and checks its internal and external links.
+ *
+ * @param f file to check
+ * @return headings found in a file
+ */
+Set<String> processInternal ( File f )
 {
-    println "Processing [$f]"
-    f
+    assert f.file
+
+    println "Processing internal links of [$f]"
+    String text               = f.getText( 'UTF-8' ).replaceAll( '\uFEFF', '' )
+    Set<String> headings      = text.findAll( /(?m)^\s*=+\s*([^=]+)\s*=+\s*$/ ){       // "= Text =" => "Text"
+        it[ 1 ].trim().
+        replaceAll( /\[.+?\s+(.+?)\]/ ){ it[ 1 ] }.                                  // [url Text1], [url Text2] => Text1, Text2
+        replaceAll( /'{2,}|<code>|<\/code>/, ''  )                                   // <code>'''Text'''</code>  => Text
+    } as Set
+
+    Set<String> internalLinks = text.findAll( /\[\[#(.+?)\|.+?\]\]/ ){ it[ 1 ] } as Set // [[#Text|description]] => Text
+    assert    ( internalLinks || ( ! text.contains( '[[#' )))
+
+    for ( link in internalLinks )
+    {
+        assert headings.contains( link ), "Internal link [$link] references non-existing section"
+    }
+
+    headings
+}
+
+
+
+void processExternal( File f, Map<String, Set<String>> headings )
+{
+    assert f.file && headings
+
+    println "Processing external links of [$f]"
+    String      text          = f.getText( 'UTF-8' ).replaceAll( '\uFEFF', '' )
+    Set<String> externalLinks = text.findAll( /\[(http.+?)\s+.+?\]/ ){ it[ 1 ] } as Set // [url text] => url
+    assert    ( externalLinks || ( ! text.contains( '[http' )))
+
+    for ( link in externalLinks.findAll { it.contains( '#' ) } )
+    {
+        def ( String fileName, String anchor ) = link.findAll( /\/([^\/]+)#(.+?)$/ ){ it[ 1, 2 ] }[ 0 ]
+        if ( headings.containsKey( fileName ))
+        {
+            assert headings[ fileName ].contains( anchor ), "External link [$fileName#$anchor] references non-existing section"
+        }
+    }
 }
