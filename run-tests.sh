2fef1904229fff0f6111fb727569e4e5f64ac146
Evgeny Goldin <evgenyg@gmail.com>
2011-11-20 02:59:55 +0200

    "run-tests.sh" added - runs Groovy scripts tests

---
+++ run-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPTS_ROOT=`pwd`
+GROOVY='groovy -Dgroovy.grape.report.downloads=true'
+echo Root is [$SCRIPTS_ROOT]
+echo Groovy is [$GROOVY]
+
+echo Removing old files
+rm -rf $SCRIPTS_ROOT/tests ~/.groovy
+
+echo Cloning projects
+git clone    git://github.com/evgeny-goldin/wiki.git    $SCRIPTS_ROOT/tests/wiki
+svn checkout http://gmaps4jsf.googlecode.com/svn/trunk/ $SCRIPTS_ROOT/tests/gmaps4jsf
+svn checkout http://svg-edit.googlecode.com/svn/trunk/  $SCRIPTS_ROOT/tests/svg-edit
+
+cd $SCRIPTS_ROOT/src/main/groovy
+
+echo Validating MediaWiki links
+$GROOVY links.groovy $SCRIPTS_ROOT/tests/wiki "**/*.txt"
+
+echo Running mvnOp.groovy
+$GROOVY mvnOp.groovy $SCRIPTS_ROOT/tests
+
+echo Running svnOp.groovy
+$GROOVY svnOp.groovy $SCRIPTS_ROOT/tests
+
+cd $SCRIPTS_ROOT/src/test/groovy
+
+echo Running y2m tests
+$GROOVY y2m.groovy
+
+echo Removing old files
+rm -rf $SCRIPTS_ROOT/tests
