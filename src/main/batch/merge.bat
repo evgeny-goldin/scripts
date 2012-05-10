cc582e48ed7efc22202af37edf3e464cf1b34635
Evgeny Goldin <evgenyg@gmail.com>
2012-05-10 18:08:23 +0200

    merge update

--- src/main/batch/merge.bat
+++ src/main/batch/merge.bat
@@ -9,5 +9,6 @@ call git rebase master
 call git checkout master
 call git status
 call git merge dev
-call git push origin
+call git push origin master
+call git push origin dev
 call git checkout dev
