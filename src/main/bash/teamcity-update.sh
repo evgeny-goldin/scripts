5ac904c616bba041bb8cac1823c4c2d8e8d5ebdc
Evgeny Goldin <evgenyg@gmail.com>
2012-05-22 16:46:06 +0200

    Updates

--- src/main/bash/teamcity-update.sh
+++ src/main/bash/teamcity-update.sh
@@ -12,3 +12,35 @@ rm    *.war
 cd    ..
 
 echo  ========== [3] - done, stopping Tomcat ==========
+
+~/java/tomcat/bin/shutdown.sh
+sleep 20
+~/cleanup-tomcat.sh
+
+echo  ========== [4] - done, moving TeamCity ==========
+
+rm -rf                            ~/download/old/teamcity
+mkdir                             ~/download 
+mkdir                             ~/download/old 
+mkdir                             ~/download/old/teamcity 
+mv ~/java/tomcat/webapps/teamcity ~/download/old/teamcity
+mv teamcity                       ~/java/tomcat/webapps
+rm -rf                            teamcity
+
+echo  ========== [5] - done, killing remaining Tomcat process ==========
+
+kill `ps | grep java | grep org.apache.catalina.startup.Bootstrap | cut -f 3 -d ' '`
+
+echo  ========== [6] - done, starting Tomcat ==========
+
+~/cleanup-tomcat.sh
+~/java/tomcat/bin/startup.sh
+
+echo  ========== [6] - done, Tomcat started! ==========
+
+sleep 60
+free
+
+echo  ========== [7] - listing Tomcat log file ==========
+
+cat ~/java/tomcat/logs/catalina.out
