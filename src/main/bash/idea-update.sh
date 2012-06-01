1ec308ff68adb61226dbf66cbbfcedf8181f37a8
Evgeny Goldin <evgenyg@gmail.com>
2012-06-01 18:55:43 +0200

    Updates

--- src/main/bash/idea-update.sh
+++ src/main/bash/idea-update.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 # -----------------------------------------
-# Things to remember:
-# - teamcity.startup.maintenance=false
-# - Backup
+# http://buildserver/guestAuth/repository/download/bt24/lastSuccessful/teamcity-ivy.xml
 # -----------------------------------------
 
-set -e
-set -o pipefail
+ideaProcess=`tasklist | grep idea.exe`
+
+if [ "$ideaProcess" != "" ]; 
+then 
+    echo 'IDEA is running'
+    exit
+fi
+
 
 url=http://buildserver/guestAuth/repository/download/bt24/lastSuccessful
-artifactName="`curl $url/teamcity-ivy.xml | grep '.win' | cut -f 2 -d '"'`.zip"
-wget "$url/$artifactName"
+artifactName="`curl $url/teamcity-ivy.xml | grep ideaIU | grep zip | grep -v teamcity | cut -d '"' -f 2`.zip"
+
+rm -rf  idea
+mkdir   idea
+cd      idea
+wget    "$url/$artifactName"
+unzip   "$artifactName"
+rm      "$artifactName"
+cd      ..
+rm -rf  \Winny\java\idea
+mv      idea \Winny\java
