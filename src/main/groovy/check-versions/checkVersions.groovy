88b0dbe7c4ab3c3ee9422ecbc1982f8004ba7bf8
Evgeny Goldin <evgenyg@gmail.com>
2012-09-20 23:13:32 +0200

    checkVersions.groovy - Kotlin/TeamCity releases update.

--- src/main/groovy/check-versions/checkVersions.groovy
+++ src/main/groovy/check-versions/checkVersions.groovy
@@ -18,8 +18,8 @@ import java.util.zip.Adler32
  */
 
 
-final URLs = [ 'http://confluence.jetbrains.net/display/TW/Previous+Releases+Downloads'       : [ 3730817111, /(content|value)="\w+"/    ],
-               'http://repository.jetbrains.com/kotlin/org/jetbrains/kotlin/kotlin-compiler/' : [ 2501569239, /\d\d-\w+-\d{4} \d\d:\d\d/ ]]
+final URLs = [ 'http://confluence.jetbrains.net/display/TW/Previous+Releases+Downloads'       : [ 2963519938, /(content|value)="\w+"/    ],
+               'http://repository.jetbrains.com/kotlin/org/jetbrains/kotlin/kotlin-compiler/' : [ 4059162509, /(\d\d-\w+-\d{4} \d\d:\d\d)|(\d+ bytes)|(Artifactory\/\d+\.\d+\.\d+)/ ]]
 
 for ( entry in URLs )
 {
