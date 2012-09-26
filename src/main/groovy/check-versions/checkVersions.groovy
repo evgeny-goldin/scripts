fc4759185b9d9401b05e2055f72e6f89972865f5
Evgeny Goldin <evgenyg@gmail.com>
2012-09-26 10:28:23 +0200

    checkVersions.groovy - Kotlin update.

--- src/main/groovy/check-versions/checkVersions.groovy
+++ src/main/groovy/check-versions/checkVersions.groovy
@@ -3,7 +3,6 @@
 @Grab('com.google.guava:guava:13.0.1')
 import com.google.common.io.ByteStreams
 import com.google.common.io.InputSupplier
-import org.apache.ivy.plugins.repository.Repository
 
 import java.util.regex.Pattern
 import java.util.zip.Adler32
@@ -19,7 +18,7 @@ import java.util.zip.Adler32
 
 
 final URLs = [ 'http://confluence.jetbrains.net/display/TW/Previous+Releases+Downloads'       : [ 604272997,  /(content|value)="\w+"/    ],
-               'http://repository.jetbrains.com/kotlin/org/jetbrains/kotlin/kotlin-compiler/' : [ 4059162509, /(\d\d-\w+-\d{4} \d\d:\d\d)|(\d+ bytes)|(Artifactory\/\d+\.\d+\.\d+)/ ]]
+               'http://repository.jetbrains.com/kotlin/org/jetbrains/kotlin/kotlin-compiler/' : [ 3333616075, /(\d\d-\w+-\d{4} \d\d:\d\d)|(\d+ bytes)|(Artifactory\/\d+\.\d+\.\d+)/ ]]
 
 for ( entry in URLs )
 {
