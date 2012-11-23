8a71210a1554566de3f7200e237664f6adba7546
Evgeny Goldin <evgenyg@gmail.com>
2012-11-23 01:24:25 +0100

    TC update - kill -9 added

--- src/main/bash/teamcity-update.sh
+++ src/main/bash/teamcity-update.sh
@@ -63,17 +63,24 @@ mv teamcity                 $tomcat/webapps
 echo  ========== Killing remaining Java processes ==========
 
 echo "Java processes before:"
-echo  [`ps -AF | grep java | grep -v grep`]
+echo  [`ps -Af | grep java | grep -v grep`]
 
-ps -AF | grep java | grep -v grep | awk '{print $2}' | while read pid;
+ps -Af | grep java | grep -v grep | awk '{print $2}' | while read pid;
 do 
     echo "kill $pid"
     kill $pid
 done
 
 sleep 10
+
+ps -Af | grep java | grep -v grep | awk '{print $2}' | while read pid;
+do 
+    echo "kill -9 $pid"
+    kill -9 $pid
+done
+
 echo "Java processes after:"
-echo  [`ps -AF | grep java | grep -v grep`]
+echo  [`ps -Af | grep java | grep -v grep`]
 
 echo  ========== Starting Tomcat ==========
 
--- src/main/bash/tm-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-scp -r bu@176.9.56.7:/home/backup/tm-backups/latest ~/Dropbox/Backup/data/trademob
