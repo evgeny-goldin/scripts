2fdd8146db684a2b1fb4e52095badd89049ffd87
Evgeny Goldin <evgenyg@gmail.com>
2012-12-09 21:57:18 +0100

    backup-sync.sh added

---
+++ src/main/bash/backup-sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -x
+
+dropbox=/Users/evgenyg/Dropbox
+data=/Volumes/Data
+backup=/Volumes/Backup
+
+if [ -d "$dropbox" -a -d "$data" -a -d "$backup" ]; then
+    rm    -rf "$dropbox/.dropbox.cache"
+    rsync -rv "$dropbox"          "$backup"
+    rsync -rv "$data/Books"       "$backup"
+    rsync -rv "$data/Tr/TheBest/" "$backup"
+fi
