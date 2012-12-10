7bf7d94ebece6dce7e522b510605c31aeda9919d
Evgeny Goldin <evgenyg@gmail.com>
2012-12-10 07:34:32 +0100

    backup-sync.sh update

--- src/main/bash/backup-sync.sh
+++ src/main/bash/backup-sync.sh
@@ -10,9 +10,9 @@ backup=/Volumes/Backup
 
 if [ -d "$dropbox" -a -d "$data" -a -d "$backup" ]; then
     rm    -rf "$dropbox/.dropbox.cache"
-    rsync -rv "$dropbox"          "$backup"
-    rsync -rv "$data/Books"       "$backup"
-    rsync -rv "$data/Tr/TheBest/" "$backup"
+    rsync -rv "$dropbox"         "$backup"
+    rsync -rv "$data/Books"      "$backup"
+    rsync -rv "$data/Tr/TheBest" "$backup"
 else
     echo Not all folders are available
     echo $dropbox
