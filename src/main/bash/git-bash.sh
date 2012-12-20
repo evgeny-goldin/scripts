4a3570982ffc3da4554b9b39bfb61ebadf2f05ed
Evgeny Goldin <evgenyg@gmail.com>
2012-12-20 16:16:31 +0100

    git-bash.sh update

--- src/main/bash/git-bash.sh
+++ src/main/bash/git-bash.sh
@@ -4,15 +4,23 @@ branch=''
 mark=''
 
 status=`git status 2> /dev/null`
-regex="nothing to commit, working directory clean$"
+pushRegex="# Your branch is ahead of"
+cleanRegex="nothing to commit, working directory clean$"
 
 if [ "$status" != "" ];
 then
     branch=`echo $status | head -1 | awk '{print $4}'`
-    if [[ ! $status =~ $regex ]]; 
+    
+    if [[ $status =~ $pushRegex ]]; 
     then
-        mark='!'    
+        mark="$mark^"
     fi
+
+    if [[ ! $status =~ $cleanRegex ]]; 
+    then
+        mark="$mark!"
+    fi
+
     echo "<$branch$mark>"
 else
     echo ""
