c1aa3d45d71e433bd7901fefcde577b158b9aa6b
Evgeny Goldin <evgenyg@gmail.com>
2013-01-29 20:56:30 +0100

    Mediawiki scripts dependency update

--- src/main/groovy/mediawiki/spaces.groovy
+++ src/main/groovy/mediawiki/spaces.groovy
@@ -1,7 +1,8 @@
 
 @GrabResolver( name='evgenyg.artifactoryonline.com', root='http://evgenyg.artifactoryonline.com/evgenyg/repo/' )
-@Grab('com.github.goldin:gcommons:0.6-SNAPSHOT')
+@Grab('com.github.goldin:gcommons:0.6.3-SNAPSHOT')
 @GrabExclude('xml-apis:xml-apis')
+@GrabExclude('org.sonatype.sisu.inject:cglib')
 import com.github.goldin.gcommons.GCommons
 
 
