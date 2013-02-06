0078e4dcaf86ffaf71609758e03fe96f95f92f48
Evgeny Goldin <evgenyg@gmail.com>
2013-02-07 00:08:45 +0100

    YouTrack check version added

--- src/main/groovy/check-versions/checkVersions.groovy
+++ src/main/groovy/check-versions/checkVersions.groovy
@@ -18,6 +18,8 @@ import java.util.zip.Adler32
 final URLs = [
     'http://confluence.jetbrains.net/display/TW/Previous+Releases+Downloads'       : [ 1645850325, /(?s).+<div class="wiki-content">/,
                                                                                                    /(?s)<rdf:RDF xmlns:rdf.+/ ],
+    'http://www.jetbrains.com/youtrack/download/get_youtrack.html'                 : [ 2933863498, /(?s).+?<dt>WAR<\/dt>/,
+                                                                                                   /(?s)<dt class="gray">.+/ ],
     'http://repository.jetbrains.com/kotlin/org/jetbrains/kotlin/kotlin-compiler/' : [ 2352484388, /(\d\d-\w+-\d{4} \d\d:\d\d)|(\d+ bytes)/,
                                                                                                    /(Artifactory\/\d+\.\d+\.\d+)/ ],
     'http://services.gradle.org/distributions'                                     : [ 1158170063, /(?s)^.+?<ul class="items">/,
