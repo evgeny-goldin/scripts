7c6bbab3b717aab35721081f55d8259cdb813fe7
Evgeny Goldin <evgenyg@gmail.com>
2015-07-25 22:06:45 -0700

    Scripts updates

--- src/main/bash/disconnect.sh
+++ src/main/bash/disconnect.sh
@@ -7,17 +7,20 @@ date
 echo '==================='
 
 result_file=~/Temp/whoer.txt
+wget='/usr/local/bin/wget'
+resource='http://whoer.net/'
+provider='Comcast'
 
 if [ "$(\ps -Af | grep -i torrent | grep -v grep | wc -l | awk '{print $1}')" == "0" ]; then
   echo '===> Torrents are not running'
-else  
+else
   echo '===> Torrents are running'
   rm -f "$result_file"
-  wget --timeout=5 -nv http://whoer.net/ -O "$result_file"
-  if [ "$(grep Comcast "$result_file" | wc -l | awk '{print $1}')" == "0" ]; then
-    echo "===> Comcast not found in '$result_file'"  
+  "$wget" --timeout=5 -nv "$resource" -O "$result_file"
+  if [ "$(grep "$provider" "$result_file" | wc -l | awk '{print $1}')" == "0" ]; then
+    echo "===> $provider not found in '$result_file'"
   else
-    echo "===> !!! Comcast found in '$result_file', turning the network off"
+    echo "===> !!! $provider found in '$result_file', turning the network off"
     /usr/sbin/scselect Off
   fi
 fi
