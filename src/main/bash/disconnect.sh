d1b47c01eac414c39ce2c9a63d520edc60cdb5a5
Evgeny Goldin <evgenyg@gmail.com>
2015-07-25 21:56:41 -0700

    Script updates

--- src/main/bash/disconnect.sh
+++ src/main/bash/disconnect.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# set -x
 set -e
 
 echo '==================='
@@ -9,16 +8,16 @@ echo '==================='
 
 result_file=~/Temp/whoer.txt
 
-if [ "$(\ps -Af | grep pia_manager | grep -v grep | grep -v grep | wc -l | awk '{print $1}')" == "0" ]; then
-  echo '===> PIA Manager is not running'
+if [ "$(\ps -Af | grep -i torrent | grep -v grep | wc -l | awk '{print $1}')" == "0" ]; then
+  echo '===> Torrents are not running'
 else  
-  echo '===> PIA Manager is running'
+  echo '===> Torrents are running'
   rm -f "$result_file"
   wget --timeout=5 -nv http://whoer.net/ -O "$result_file"
   if [ "$(grep Comcast "$result_file" | wc -l | awk '{print $1}')" == "0" ]; then
     echo "===> Comcast not found in '$result_file'"  
   else
-    echo "===> Comcast found in '$result_file'! Turning network off"  
+    echo "===> !!! Comcast found in '$result_file', turning the network off"
     /usr/sbin/scselect Off
   fi
 fi
