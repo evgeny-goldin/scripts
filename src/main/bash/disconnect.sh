1c18572d2b65dfae05f2c59bbf35e59eebcabc95
Evgeny Goldin <evgenyg@gmail.com>
2015-07-25 21:21:26 -0700

    src/main/bash/disconnect.sh added

---
+++ src/main/bash/disconnect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+
+result_file=~/Temp/whoer.txt
+
+if [ "$(\ps -Af | grep pia_manager | grep -v grep | grep -v grep | wc -l | awk '{print $1}')" == "0" ]; then
+  echo '===> PIA Manager is not running'
+else  
+  echo '===> PIA Manager is running'
+  rm -f "$result_file"
+  wget --timeout=5 -nv http://whoer.net/ -O "$result_file"
+  if [ "$(grep Comcast "$result_file" | wc -l | awk '{print $1}')" == "0" ]; then
+    echo "===> Comcast not found in '$result_file'"  
+  else
+    echo "===> Comcast found in '$result_file'! Turning network off"  
+    /usr/sbin/scselect Off
+  fi
+fi
