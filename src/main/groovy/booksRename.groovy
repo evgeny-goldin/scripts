437f1cfd52cf7c905e4d6d750954dcda57e647ff
Evgeny Goldin <evgenyg@gmail.com>
2016-01-12 12:13:43 -0800

    booksRename update

--- src/main/groovy/booksRename.groovy
+++ src/main/groovy/booksRename.groovy
@@ -1,17 +1,18 @@
 
-def publishers = '''Addison.Wesley Apress Artima AWP Big.Nerd For.Dummies Leanpub PP FriendsofED FT.Press IBM.Press Manning Microsoft.Press New.Riders No.Starch.Press
+final publishers = '''Addison.Wesley Apress Packt Artima AWP Big.Nerd For.Dummies Leanpub PP FriendsofED FT.Press IBM.Press Manning Microsoft.Press New.Riders No.Starch.Press
                     No.Starch Oreilly OReilly Pactpub Packtpub Peachpit Peachpit.Press Pragmatic Prentice.Hall Prentice.Hall SitePoint Sitepoint QUE Sams Wiley Wrox'''.
                     stripIndent().tokenize()
-def months     = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.tokenize()
-def extensions = 'pdf epub'.tokenize()
+final months     = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.tokenize()
+final filter     = ( publishers + months ).collect{ /\Q$it.\E/ }.join( '|' )
 
-new File( '.' ).listFiles().findAll { File f -> f.file && extensions.any { f.name.endsWith( it ) }}.
+new File( '.' ).listFiles().findAll { File f -> f.file && f.name.endsWith('.pdf') }.
                             each    { File f ->
                                 def ( body, extension ) = f.name.findAll( /^(.+)\.([^\.]+)$/ ){ it[1,2] }[ 0 ]
                                 def newBody =
-                                    body.replaceAll  ( /\d{4}/, '' ).
-                                         replaceAll  (( publishers + months ).collect{ /\Q$it.\E/ }.join( '|' ), '' ).
-                                         replace     ( '.',    ' '    ).
+                                    body.replaceAll  ( /\d{10}/, '' ).
+                                         replaceAll  ( /\d{4}/,  '' ).
+                                         replaceAll  ( filter,   '' ).
+                                         replaceAll  ( /\.+/,    ' ').
                                          replaceAll  ( /\s*(\d+)(nd|rd|th) Edition/, ', $1Ed' ).
                                          trim()
 
