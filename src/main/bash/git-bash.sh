f2edac58d17d0154416068de52c23d701a480c9a
Evgeny Goldin <goldin@amazon.com>
2016-07-30 22:40:32 -0700

    git-bash.sh - selecting origin/master or origin/mainline

--- src/main/bash/git-bash.sh
+++ src/main/bash/git-bash.sh
@@ -10,6 +10,7 @@ noBranchRegex2="HEAD detached at"
 divergedRegex="have diverged"
 pushRegex="# Your branch is ahead of"
 cleanRegex="nothing to commit, working tree clean"
+main_branch="origin/master"
 
 if [ "$status" != "" ];
 then
@@ -41,6 +42,12 @@ then
 
     curr_branch=$(git rev-parse --abbrev-ref HEAD 2>&1);
 
+    for br in $(git branch); do
+      if [[ "$br" == "mainline" ]]; then
+        main_branch="origin/mainline"
+      fi
+    done
+
     if [[ "$curr_branch" =~ "fatal: " ]]; then
       echo "<not initialized yet>"
       exit
@@ -51,7 +58,7 @@ then
 
     if [ "$curr_remote" == "" ] || [ "$curr_merge_branch" == "" ] ;
     then
-        numbers=$(git rev-list --left-right --count $curr_branch...origin/master 2>&1 | tr -s '\t' '|');
+        numbers=$(git rev-list --left-right --count $curr_branch...$main_branch 2>&1 | tr -s '\t' '|');
     else
         numbers=$(git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch 2>&1 | tr -s '\t' '|');
     fi
